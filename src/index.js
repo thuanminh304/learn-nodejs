c28886e9212fce26b0640b8eaf47f9b715bdceb1
thuanvm <thuanminh304@gmail.com>
2021-10-07 20:32:56 +0700

    'scss-config'

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const morgan = require("morgan");
+const exphbs=require('express-handlebars')
+const path=require('path')
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname,'public')))
+
+//HTTP logger
+app.use(morgan("combined"));
+
+//template engine
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'resources/views'))
+
+/////
+app.get("/", (req, res) => {
+  res.render('home');
+});
+
+app.get("/news", (req, res) => {
+  res.render('news');
+});
+app.get("/contact", (req, res) => {
+  res.render('contact');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
